4eae0690db96c9614b9cbad86599ce5bd9030dce
Daniela <danimoli0996@gmail.com>
2024-11-02 19:00:34 -0500

    Cambio de ruta de admin por superadmin

--- salon/urls.py
+++ salon/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('superadmin/', admin.site.urls),
     path('hola-mundo/', views.holaMundo,name="holaMundo" ),
     path('', include('servicios.urls')),
 ]
